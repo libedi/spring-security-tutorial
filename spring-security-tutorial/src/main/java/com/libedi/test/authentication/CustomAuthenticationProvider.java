3ca65433351e3f5f814aea6e14f9460efbc0e702
libedi <libedi@gmail.com>
2015-05-27 23:03:31 +0900

    customizing spring security

---
+++ spring-security-tutorial/src/main/java/com/libedi/test/authentication/CustomAuthenticationProvider.java
@@ -0,0 +1,46 @@
+package com.libedi.test.authentication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.libedi.test.user.CustomUserDetails;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = (String) authentication.getPrincipal();
+		String password = (String) authentication.getCredentials();
+		
+		// check whether user's credentials are valid.
+		// if false, throw new BadCredentialsException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials", "Bad credentials"));
+		// DB를 조회하여 인증
+		if(false){
+			throw new BadCredentialsException("Bad credentials");
+		}
+		
+		// 인증이 성공하면 User 클래스를 반환
+		// 사용자의 권한 테이블에서 값을 가져와 셋팅할 수 있다.
+		List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+		roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+		UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(username, password, roles);
+		result.setDetails(new CustomUserDetails(username, password, roles));
+		
+		return result;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
---
+++ spring-security-tutorial/src/main/webapp/WEB-INF/views/loginSuccess.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+Login Success!
+<button type="button" onclick="javascript:location.href='/test/logout';">logout</button>
+</body>
+</html>
